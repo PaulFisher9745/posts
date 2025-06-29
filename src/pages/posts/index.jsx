2b506b3277841557a61262f64382b270590e5635
Paul <paulfisher9745@gmail.com>
2025-06-29 16:08:13 +0700

    add redux

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,43 +1,20 @@
-import React from 'react'
+import React  from 'react'
 import Container from '../../components/Container'
 import Posts from '../../components/Posts'
 import Typo from '../../components/Typo'
+import { useSelector } from 'react-redux'
 
-export const INITIAL_POSTS = [
-  {
-    id:1,
-    title: "post 1",
-    image: "https://avatars.mds.yandex.net/i?id=2a00000197a64025d8aa1821cb298414b8ee-1767271-fast-images&n=13",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima molestiae laborum expedita eius natus adipisci asperiores, possimus placeat commodi nobis sit recusandae aliquid doloremque blanditiis fugiat quasi ratione aspernatur at!"
-  },
-   {
-    id:2,
-    title: "post 2",
-    image: "https://avatars.mds.yandex.net/i?id=2a00000197a64025d8aa1821cb298414b8ee-1767271-fast-images&n=13",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima molestiae laborum expedita eius natus adipisci asperiores, possimus placeat commodi nobis sit recusandae aliquid doloremque blanditiis fugiat quasi ratione aspernatur at!"
-  },
-   {
-    id:3,
-    title: "post 3",
-    image: "https://avatars.mds.yandex.net/i?id=2a00000197a64025d8aa1821cb298414b8ee-1767271-fast-images&n=13",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima molestiae laborum expedita eius natus adipisci asperiores, possimus placeat commodi nobis sit recusandae aliquid doloremque blanditiis fugiat quasi ratione aspernatur at!"
-  },
-   {
-    id:4,
-    title: "post 4",
-    image: "https://avatars.mds.yandex.net/i?id=2a00000197a64025d8aa1821cb298414b8ee-1767271-fast-images&n=13",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima molestiae laborum expedita eius natus adipisci asperiores, possimus placeat commodi nobis sit recusandae aliquid doloremque blanditiis fugiat quasi ratione aspernatur at!"
-  },
-]
 
 const PostsPage = () => {
+  const posts = useSelector((state) => {
+    return state.posts.list
+  })
+
   return (
-    <>
         <Container>
             <Typo>Публикации</Typo>
-            <Posts posts={INITIAL_POSTS}/>
-        </Container>
-    </>  
+            <Posts posts={posts}/>
+        </Container> 
   )
 }
 
